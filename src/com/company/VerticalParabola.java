c92b78b6b2d97c382d5d622861d25c9c433dad34
SifySM <90615357+SifySM@users.noreply.github.com>
2021-11-02 20:09:34 +0300

    Delete src/com/company directory

--- src/com/company/VerticalParabola.java
+++
@@ -1,7 +0,0 @@
-package com.company;
-
-public record VerticalParabola(double a, double b, double c) {
-    public boolean pointIsAbove(Point point) {
-        return point.y >= (a * point.x + b) * point.x + c;
-    }
-}
