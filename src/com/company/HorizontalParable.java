a5e9327ea9c3c9f4d6be877314f0c2e3f7b51f53
SifySM <90615357+SifySM@users.noreply.github.com>
2021-11-10 15:11:14 +0300

    Delete src/com/company directory

--- src/com/company/HorizontalParable.java
+++
@@ -1,7 +0,0 @@
-package com.company;
-
-public record HorizontalParable(double a, double b, double c) {
-    public boolean pointIsOnRight(double x, double y) {
-        return x >= (a * y + b) * y + c;
-    }
-}
--- src/com/company/VerticalParabola.java
+++
@@ -1,7 +0,0 @@
-package com.company;
-
-public record VerticalParabola(double a, double b, double c) {
-    public boolean pointIsAbove(double x, double y) {
-        return y >= (a * x + b) * x + c;
-    }
-}
